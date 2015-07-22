5cef8bd895945a7fa4f435ea38edef97c040201d
Scott Kinder <kinder.scott@gmail.com>
2015-07-22 11:15:57 -0600

    Adding verify=False to saw3.

--- saw3.py
+++ saw3.py
@@ -220,5 +220,5 @@ def test_target(auth_token, target, url=base_url):
     '''
     headers = {'Accept': 'application/x-yaml', 'X-Auth-Token': auth_token}
     data = {'client': 'local', 'tgt': target, 'fun': 'test.ping'}
-    r = requests.post(url, headers=headers, data=data)
+    r = requests.post(url, headers=headers, data=data, verify=False)
     return r.content
